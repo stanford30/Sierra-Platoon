2d8acc1cd2dbd43e3795c6c1e7c0c21dee7c17ec
stanford30 <stanleyzheng84@gmail.com>
2022-09-30 15:17:28 -0500

    .

--- algo-99-bottles/99_bottles.py
+++ algo-99-bottles/99_bottles.py
@@ -4,9 +4,15 @@ def bottle_song():
     arr = []
 
     while (bottles > 1):
-        string = f'{bottles} bottles of beer on the wall, {bottles} of beer. Take one down and pass it around, {bottles-1} bottles of beer on the wall.'
+        if bottles == 2:
+            bottle = 'bottle'
+        else:
+            bottle = 'bottles'
+        string = f'{bottles} bottles of beer on the wall, {bottles} of beer. Take one down and pass it around, {bottles-1} {bottle} of beer on the wall.'
         arr.append(string)
         bottles -= 1
+
+    print('\n'.join(arr))
 
 
 bottle_song()
