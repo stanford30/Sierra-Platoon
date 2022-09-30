3d0fecb3ed08e57592b47003ce230d9a3d0f547f
stanford30 <stanleyzheng84@gmail.com>
2022-09-30 15:21:08 -0500

    python solution

--- algo-99-bottles/99_bottles.py
+++ algo-99-bottles/99_bottles.py
@@ -4,13 +4,15 @@ def bottle_song():
     arr = []
 
     while (bottles > 1):
+        bottle = 'bottles'
         if bottles == 2:
             bottle = 'bottle'
-        else:
-            bottle = 'bottles'
+
         string = f'{bottles} bottles of beer on the wall, {bottles} of beer. Take one down and pass it around, {bottles-1} {bottle} of beer on the wall.'
         arr.append(string)
         bottles -= 1
+    string = "1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, no more bottles of beer on the wall.\n No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall."
+    arr.append(string)
 
     print('\n'.join(arr))
 
