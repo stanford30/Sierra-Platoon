acf157a5a81058072124662ac08e1358c1c2da87
stanford30 <stanleyzheng84@gmail.com>
2022-10-01 01:31:09 -0500

    deaf grandma js

--- week-01/day-01/algo-deaf-grandma/deaf_grandma.js
+++ week-01/day-01/algo-deaf-grandma/deaf_grandma.js
@@ -1,5 +1,27 @@
 function deafGrandma() {
+  let running = true;
+  let goodbye = 0;
+  while (running) {
+    let x = prompt();
+    console.log(x);
+    x = x.toString();
 
+    if (x === 'GOODBYE!') {
+      if (goodbye < 1) {
+        console.log('LEAVING SO SOON?');
+        goodbye++;
+      } else {
+        console.log('LATER, SKATER!');
+        running = false;
+      }
+    } else if (x == '') {
+      console.log('WHAT?!');
+    } else if (x.toUpperCase() === x && !Number.isInteger(+x)) {
+      console.log('NO, NOT SINCE 1964!');
+    } else {
+      console.log('SPEAK UP, KID!');
+    }
+  }
 }
 
 deafGrandma();
