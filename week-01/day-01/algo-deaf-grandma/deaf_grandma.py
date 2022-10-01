4405cfbfb8d49a3b6541f941ab2012702635db73
stanford30 <stanleyzheng84@gmail.com>
2022-09-30 23:23:58 -0500

    deaf grandma python

--- week-01/day-01/algo-deaf-grandma/deaf_grandma.py
+++ week-01/day-01/algo-deaf-grandma/deaf_grandma.py
@@ -1,5 +1,23 @@
 def deaf_grandma():
+    print('HEY KID')
+    running = True
+    goodbye = 0
+    while running:
+        x = input()
+        if x == 'GOODBYE!':
+            if goodbye == 1:
+                print('LATER, SKATER!')
+                running = False
+            else:
+                print('LEAVING SO SOON?')
+                goodbye += 1
+        elif x.isupper():
+            print('NO, NOT SINCE 1964!')
+        elif x == '':
+            print('WHAT?!')
 
+        else:
+            print('SPEAK UP, KID!')
 
 
 deaf_grandma()
