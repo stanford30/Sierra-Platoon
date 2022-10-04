76dd94536a60247bdc6c2e44f209a01b87f55170
stanford30 <stanleyzheng84@gmail.com>
2022-10-04 16:59:38 -0500

    factorial prob sol

--- week-01/day-02/algo-factorial/js/factorial.js
+++ week-01/day-02/algo-factorial/js/factorial.js
@@ -1,2 +1,9 @@
-exports.factorial = function(num) {
+const helper = (n) => {
+  if (!n || n === 1) return 1;
+  return n * helper(n - 1);
+};
+
+exports.factorial = function (num) {
+  return helper(num);
+  // return num * exports.factorial(num--);
 };
--- week-01/day-02/algo-factorial/python/factorial.py
+++ week-01/day-02/algo-factorial/python/factorial.py
@@ -1,2 +1,6 @@
 def factorial(num):
-	# your code here
+    # your code here
+    if num == 0 or num == 1:
+        return 1
+
+    return num * factorial(num - 1)
