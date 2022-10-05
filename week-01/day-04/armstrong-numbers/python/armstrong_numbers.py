72f96e34d5084bdaae86cfe47a877d6c21feb5cd
stanford30 <stanleyzheng84@gmail.com>
2022-10-05 16:18:37 -0500

    .gitignore

--- week-01/day-04/armstrong-numbers/python/armstrong_numbers.py
+++ week-01/day-04/armstrong-numbers/python/armstrong_numbers.py
@@ -1,2 +1,13 @@
 # How can you make this more scalable and reusable later?
 def find_armstrong_numbers(numbers):
+    ans = []
+    for number in numbers:
+        number = str(number)
+        sum = 0
+        length = len(number)
+        for a in number:
+            sum += int(a)**length
+        if sum == int(number):
+            ans.append(sum)
+
+    return ans
