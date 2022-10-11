5aa19b1d85f3d6a1ece731ff43b082cadfff50a0
stanford30 <stanleyzheng84@gmail.com>
2022-10-11 15:59:07 -0500

    caesar_cipher sol

--- w2/d2/caesar-cipher/python/caesar_cipher.py
+++ w2/d2/caesar-cipher/python/caesar_cipher.py
@@ -1,1 +1,18 @@
 def caesar_cipher(string, shift_amount):
+    abc = "abcdefghijklmnopqrstuvwxyz"
+    ans = ""
+    for x in string:
+        try:
+            shift = abc.index(x.lower()) + shift_amount
+            if shift > 25:
+                shift -= 26
+            substr = abc[shift]
+            if x.isupper():
+                substr = substr.upper()
+            ans += substr
+        except ValueError:
+            ans += x
+    return ans
+
+
+caesar_cipher("Abcdefghijklmnopqrstuvwxyz", 5)
