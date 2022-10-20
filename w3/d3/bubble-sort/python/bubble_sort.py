1d583e48903b0f82be158834c7c3323282900f1e
stanford30 <stanleyzheng84@gmail.com>
2022-10-19 21:40:42 -0500

    bubble-sort sol

--- w3/d3/bubble-sort/python/bubble_sort.py
+++ w3/d3/bubble-sort/python/bubble_sort.py
@@ -1,7 +1,22 @@
-sequence = [4, 3, 5, 0, 1]
+sequence = [4, 3, 5, 0, 1, 0, 0, 0, 10, 0, 2, 1, 3, 4, 5]
 swaps = 0
 
 # Your Code Here
+prev = 0
+curr = 1
+while curr < len(sequence):
+    prev_num = sequence[prev]
+    curr_num = sequence[curr]
+    if curr_num < prev_num:
+        sequence[prev] = curr_num
+        sequence[curr] = prev_num
+        swaps += 1
+        if prev != 0:
+            prev -= 1
+            curr -= 1
+    elif curr_num >= prev_num:
+        curr += 1
+        prev += 1
 
-print(f"Final result: {result}")
+print(f"Final result: {sequence}")
 print(f"Swaps: {swaps}")
