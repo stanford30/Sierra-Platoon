c0701b3536044b924b90b3cba299e664cdb19742
stanford30 <stanleyzheng84@gmail.com>
2022-10-21 13:29:23 -0500

    d5

--- w3/d3/bubble-sort/python/bubble_sort.py
+++ w3/d3/bubble-sort/python/bubble_sort.py
@@ -1,22 +1,37 @@
-sequence = [4, 3, 5, 0, 1, 0, 0, 0, 10, 0, 2, 1, 3, 4, 5]
+sequence = [4, 3, 5, 0, 1]
 swaps = 0
 
 # Your Code Here
-prev = 0
-curr = 1
-while curr < len(sequence):
-    prev_num = sequence[prev]
-    curr_num = sequence[curr]
-    if curr_num < prev_num:
-        sequence[prev] = curr_num
-        sequence[curr] = prev_num
-        swaps += 1
-        if prev != 0:
-            prev -= 1
-            curr -= 1
-    elif curr_num >= prev_num:
-        curr += 1
-        prev += 1
+# prev = 0
+# curr = 1
+# while curr < len(sequence):
+#     prev_num = sequence[prev]
+#     curr_num = sequence[curr]
+#     if curr_num < prev_num:
+#         sequence[prev] = curr_num
+#         sequence[curr] = prev_num
+#         swaps += 1
+#         print(sequence)
+#         if prev != 0:
+#             prev -= 1
+#             curr -= 1
+#     elif curr_num >= prev_num:
+#         curr += 1
+#         prev += 1
+
+
+swapped = True
+while swapped:
+    swapped = False
+    for i in range(len(sequence) - 1):
+        if sequence[i] > sequence[i + 1]:
+            prev = sequence[i]
+            sequence[i] = sequence[i + 1]
+            sequence[i + 1] = prev
+            swapped = True
+            swaps += 1
+        print(sequence)
+
 
 print(f"Final result: {sequence}")
 print(f"Swaps: {swaps}")
