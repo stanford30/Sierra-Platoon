874a323afe80878c4f8f42878272c3f7af77cc0f
stanford30 <stanleyzheng84@gmail.com>
2022-10-30 13:52:36 -0500

    html-simple-to-do

--- w4/d5/hello-django/config/urls.py
+++ w4/d5/hello-django/config/urls.py
@@ -23,24 +23,39 @@ def rectangleArea(request):
     # print(dir(request))
     # print(request.META["QUERY_STRING"])
     # print(request.GET.get("name"))
-    length = int(request.GET.get("length"))
-    width = int(request.GET.get("width"))
+    try:
+        length = int(request.GET.get("length"))
+        width = int(request.GET.get("width"))
+    except:
+        return badResponse()
+
     area = length * width
     return HttpResponse(area)
 
 
 def rectanglePerimeter(request):
-    length = int(request.GET.get("length"))
-    width = int(request.GET.get("width"))
+    try:
+        length = int(request.GET.get("length"))
+        width = int(request.GET.get("width"))
+    except:
+        response = HttpResponse()
+        response.status_code = 400
+        return response
+
     p = 2 * (length + width)
     return HttpResponse(p)
 
 
 def circleArea(request):
+    try:
+        radius = int(request.GET.get("radius"))
+    except:
+        return badResponse()
 
-    radius = int(request.GET.get("radius"))
     area = math.pi * radius**2
-    return HttpResponse(area)
+    response = HttpResponse(f"area = {area}")
+    response.status_code = 200
+    return response
 
 
 def circlePerimeter(request):
@@ -54,10 +69,45 @@ def circlePerimeter(request):
     return HttpResponse(f"circumference: {circumference}")
 
 
+def badResponse():
+    response = HttpResponse()
+    response.status_code = 400
+    return response
+
+
+def rectangleAreaWithParams(request, height, width):
+    area = height * width
+    response = HttpResponse(f"area={area}")
+    response.status_code = 200
+    return response
+
+
+def rectanglePerimeterWithParams(request, height, width):
+    perimeter = 2 * (height + width)
+    response = HttpResponse(f"perimeter={perimeter}")
+    response.status_code = 200
+    return response
+
+
+def circleAreaWithParams(request, radius):
+    area = radius**2 * math.pi
+    response = HttpResponse(f"area={area}")
+    response.status_code = 200
+    return response
+
+
+"""
+/rectangle/perimeter/<int:height>/<int:width>
+/circle/area/<int:radius>
+/circle/perimeter/<int:radius>
+"""
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("rectangle/area", rectangleArea),
     path("rectangle/perimeter", rectanglePerimeter),
     path("circle/area", circleArea),
     path("circle/perimeter", circlePerimeter),
+    path("rectangle/area/<int:height>/<int:width>", rectangleAreaWithParams),
+    path("rectangle/perimeter/<int:height>/<int:width>", rectanglePerimeterWithParams),
+    path("circle/area/<int:radius>", circleAreaWithParams),
 ]
