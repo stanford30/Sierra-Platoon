de395a84cb921a53e0f9b451525b2f23f0218906
stanford30 <stanleyzheng84@gmail.com>
2022-11-20 11:01:05 -0600

    doors-game

--- w7/d4/doors-game/src/components/Door.js
+++ w7/d4/doors-game/src/components/Door.js
@@ -1,36 +1,78 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import GiftImage from '../images/gift.png';
 
-function Door(props) {
-  // states
-  const [opened, setOpened] = useState(false);
+class Door extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { opened: false };
+    this.toggleDoor = this.toggleDoor.bind(this);
+    this.getDoorStateStyle = this.getDoorStateStyle.bind(this);
+    this.renderImage = this.renderImage.bind(this);
+  }
 
-  // events
-  const toggleDoor = () => {
-    if (props.result !== null) return;
-    setOpened((prevOpened) => {
-      return !prevOpened;
-    });
-    props.updateResult(props.number);
-  };
+  toggleDoor() {
+    console.log(this.opened);
+    console.log(this.state.opened);
 
-  // renders
-  const getDoorStateStyle = () => {
-    return ' ' + (opened ? 'door-opened' : 'door-closed');
-  };
+    if (this.props.result !== null) return;
 
-  const renderImage = () => {
-    return opened && props.isPrizeDoor ? (
+    this.setState({ opened: !this.state.opened });
+    this.props.updateResult(this.props.number);
+  }
+
+  getDoorStateStyle() {
+    return ' ' + (this.state.opened ? 'door-opened' : 'door-closed');
+  }
+
+  renderImage() {
+    return this.state.opened && this.props.isPrizeDoor ? (
       <img id="image-prize" src={GiftImage} alt="prize" />
     ) : null;
-  };
+  }
 
-  return (
-    <div className={'door ' + getDoorStateStyle()} onClick={toggleDoor}>
-      <div>Door {props.number}</div>
-      {renderImage()}
-    </div>
-  );
+  render() {
+    return (
+      <div
+        className={'door ' + this.getDoorStateStyle()}
+        onClick={this.toggleDoor}
+      >
+        <div>Door {this.props.number}</div>
+        {this.renderImage()}
+      </div>
+    );
+  }
 }
+
+// function Door(props) {
+//   // states
+//   const [opened, setOpened] = useState(false);
+
+//   // events
+//   const toggleDoor = () => {
+//     if (props.result !== null) return;
+//     setOpened((prevOpened) => {
+//       return !prevOpened;
+//     });
+//     props.updateResult(props.number);
+//   };
+
+//   // renders
+//   const getDoorStateStyle = () => {
+//     return ' ' + (opened ? 'door-opened' : 'door-closed');
+//   };
+
+//   const renderImage = () => {
+//     return opened && props.isPrizeDoor ? (
+//       <img id="image-prize" src={GiftImage} alt="prize" />
+//     ) : null;
+//   };
+
+//   return (
+//     <div className={'door ' + getDoorStateStyle()} onClick={toggleDoor}>
+//       <div>Door {props.number}</div>
+//       {renderImage()}
+//     </div>
+//   );
+// }
 
 export default Door;
