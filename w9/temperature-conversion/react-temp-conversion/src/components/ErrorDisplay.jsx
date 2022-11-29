8ef2fc299770d13fea4d6e1a8c200969a58778d6
stanford30 <stanleyzheng84@gmail.com>
2022-11-29 12:17:50 -0600

    ErrorDisplay.jsx

--- w9/temperature-conversion/react-temp-conversion/src/components/ErrorDisplay.jsx
+++ w9/temperature-conversion/react-temp-conversion/src/components/ErrorDisplay.jsx
@@ -1,21 +1,34 @@
-import { Component } from "react"
+import { Component } from "react";
+import { useEffect } from "react";
 
-class ErrorDisplay extends Component {
+const ErrorDisplay = (props) => {
+  useEffect(() => {
+    console.log("!! There was an error !!");
 
+    console.log("++ The error was resolved ++");
+  }, [props.message]);
+
+  return (
+    <div>
+      <p className="error">Error: {props.message}</p>
+    </div>
+  );
+};
+class ErrorDisplay1 extends Component {
   componentDidMount() {
-    console.log("!! There was an error !!")
+    console.log("!! There was an error !!");
   }
 
   componentWillUnmount() {
-    console.log("++ The error was resolved ++")
+    console.log("++ The error was resolved ++");
   }
 
   render() {
     return (
       <div>
-        <p className="error">Error: { this.props.message }</p>
+        <p className="error">Error: {this.props.message}</p>
       </div>
-    )
+    );
   }
 }
 
