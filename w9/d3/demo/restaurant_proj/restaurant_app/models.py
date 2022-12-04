523b2d51f26391461c8bd7a1e46b851124376b20
stanford30 <stanleyzheng84@gmail.com>
2022-12-04 11:49:57 -0600

    update restaurant_proj

--- w9/d3/demo/restaurant_proj/restaurant_app/models.py
+++ w9/d3/demo/restaurant_proj/restaurant_app/models.py
@@ -11,3 +11,16 @@ class AppUser(AbstractUser):
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+
+
+class Item(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    category = models.ForeignKey(
+        "Category", related_name="items", on_delete=models.CASCADE
+    )
